356cec1f8ab3a43f299c54e81e541ea229c84a64
unknown <lij061703@gmail.com>
2020-08-12 11:12:00 -0400

    4th

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ for (let i = 0; i < nav.length; i++) {
         nav[i].style.fontSize = "15px";
     }
 }
+
+let textbox = document.getElementById("user_text");
+let clickButton = document.querySelector("button");
+
+clickButton.onclick = function(event) {
+    event.preventDefault();
+    let userInput = textbox.value;
+
+    if (textbox.value == "BLM" || textbox.value == "discrimination" || textbox.value == "blm" || textbox.value == "Discrimination" ) {
+        window.open("/Discrimination/index.html", '_blank');
+    }
+}

ba65cddd3b01def6ca475002e03d3734566b5cba
unknown <lij061703@gmail.com>
2020-08-13 12:07:31 -0400

    test commit

--- script.js
+++ script.js
@@ -20,23 +20,23 @@ clickButton.onclick = function(event) {
 
     if (textbox.value == "BLM" || textbox.value == "discrimination" || textbox.value == "blm" || textbox.value == "Discrimination" || textbox.value == "hate" || textbox.value == "racism" || textbox.value == "Racism" || textbox.value == "Inequality" || textbox.value == "inequality" || textbox.value == "police") {
         textbox.value = "";
-        window.open("/Discrimination/index.html", '_blank');
+        window.open("Discrimination/index.html", '_blank');
     }
     else if (textbox.value == "Global warming" || textbox.value == "Global Warming" || textbox.value == "global warming" || textbox.value == "GLOBAL WARMING" || textbox.value == "sea level" || textbox.value == "Sea Level" || textbox.value == "Sea level" || textbox.value == "SEA LEVEL" || textbox.value == "temperature" || textbox.value == "Temperature" || textbox.value == "global" || textbox.value == "Global" || textbox.value == "Environmental" || textbox.value == "environmental") {
         textbox.value = "";
-        window.open("/Environmental/index.html", "_blank");
+        window.open("Environmental/index.html", "_blank");
     }
     else if (textbox.value == "covid" || textbox.value == "COVID" || textbox.value == "Covid" || textbox.value == "covid-19" || textbox.value == "Covid-19" || textbox.value == "COVID-19" || textbox.value == "pandemic" || textbox.value == "Pandemic" || textbox.value == "virus" || textbox.value == "Virus" || textbox.value == "coronavirus" || textbox.value == "Coronavirus" || textbox.value == "CORONAVIRUS") {
         textbox.value = "";
-        window.open("/Global_Pandemic/index.html", "_blank");
+        window.open("Global_Pandemic/index.html", "_blank");
     }
     else if (textbox.value == "current" || textbox.value == "Current" || textbox.value == "CURRENT" || textbox.value == "Lebanon" || textbox.value == "lebanon" || textbox.value == "LEBANON" || textbox.value == "events" || textbox.value == "EVENTS" || textbox.value == "Events" || textbox.value == "world") {
         textbox.value = "";
-        window.open("/Current/index.html", "_blank");
+        window.open("Current/index.html", "_blank");
     }
     else if (textbox.value == "petition" || textbox.value == "Petition" || textbox.value == "PETITION" || textbox.value == "goals" || textbox.value == "Goals" || textbox.value == "goal" || textbox.value == "Goal"){
         textbox.value = "";
-        window.open("/goal/index.html", "_blank");
+        window.open("goal/index.html", "_blank");
     }
     else {
         textbox.value = "Try a different word!"
